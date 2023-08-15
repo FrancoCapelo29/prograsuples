31964748d2a279c0c95b4aae0459c3279ab75876
Franco Capelo <115761575+FrancoCapelo29@users.noreply.github.com>
2023-08-15 09:59:41 -0500

    1

---
+++ index.js
@@ -0,0 +1,21 @@
+let winningticket = [2,1,1,2];
+let petwinner = "seal";
+let petloser = "tortoise";
+let petloser1 = "seal";
+let ticket = "2,1,1,2";
+
+
+
+if(winningticket==ticket){
+    console.log("Winner");
+} else if(winningticket!=ticket){
+    console.log("Loser");
+}
+
+if(petwinner==petloser){
+    console.log("Another chance");
+} else if(petwinner!=petloser){
+    console.log("Loser");
+}
+
+
